c10d5441d16120f9b504708c24f55dfa0b5e093b
Anurag-Mishra22 <anurag2025mishra@gmail.com>
2024-05-23 15:55:30 +0530

    implemented authentication

---
+++ app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,26 @@
+import { Loader2 } from "lucide-react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+    return <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 items-center">
+        <div className="h-full lg:flex flex-col items-center justify-center px-4">
+            <div className="text-center spage-y-4 pt-16">
+                <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
+
+            </div>
+            <div className="flex items-center justify-center mt-8">
+                <ClerkLoaded>
+                    <SignIn routing="hash" />
+                </ClerkLoaded>
+                <ClerkLoading>
+                    <Loader2 className="animate-spin  text-muted-foreground" />
+                </ClerkLoading>
+            </div>
+        </div>
+
+        <div className="h-full bg-blue-200 hidden lg:flex items-center justify-center">
+            <Image src="/logo.svg" height={100} width={100} alt="Logo" />
+        </div>
+    </div>
+}
---
+++ config/siteConfig.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "TaskTrack",
+  description: "Collaborate, manage projects, and reach new productivity peaks",
+};
